0a7a60cbc1fb855586913e1ce35f35f7581ceb22
pawelrychlik <pawel.rychlik@rspective.pl>
2013-04-12 18:19:55 +0200

    initial refactoring of slurper

--- src/main/java/org/elasticsearch/river/couchdb/Slurper.java
+++ src/main/java/org/elasticsearch/river/couchdb/Slurper.java
@@ -1,6 +1,7 @@
 package org.elasticsearch.river.couchdb;
 
 import static org.elasticsearch.common.base.Joiner.on;
+import static org.elasticsearch.river.couchdb.util.Sleeper.sleepLong;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.io.Closeables;
@@ -47,131 +48,132 @@ public class Slurper implements Runnable {
         return on(":").join(getClass().getSimpleName(), databaseConfig.getDatabase());
     }
 
-    @SuppressWarnings({"unchecked"})
     @Override
     public void run() {
-
-        while (true) {
-            if (closed) {
-                return;
-            }
-
-            String lastSeq = null;
+        while (!closed) {
             try {
-                client.admin().indices().prepareRefresh(riverConfig.getRiverIndexName()).execute().actionGet();
-                GetResponse lastSeqGetResponse = client.prepareGet(riverConfig.getRiverIndexName(),
-                        riverConfig.getRiverName().name(), "_seq").execute().actionGet();
-                if (lastSeqGetResponse.isExists()) {
-                    Map<String, Object> couchdbState = (Map<String, Object>) lastSeqGetResponse.getSourceAsMap().get("couchdb");
-                    if (couchdbState != null) {
-                        lastSeq = couchdbState.get("last_seq").toString(); // we know its always a string
-                    }
-                }
+                slurp();
             } catch (Exception e) {
-                logger.warn("failed to get last_seq, throttling....", e);
-                try {
-                    Thread.sleep(5000);
-                    continue;
-                } catch (InterruptedException e1) {
-                    if (closed) {
-                        return;
-                    }
-                }
+                logger.warn("Slurper error for database=[{}].", e, databaseConfig.getDatabase());
+                sleepLong("to avoid log flooding");
             }
+        }
+        logger.info("Closing " + name());
+    }
 
-            String file = "/" + databaseConfig.getDatabase() + "/_changes?feed=continuous&include_docs=true&heartbeat=10000";
-            if (databaseConfig.shouldUseFilter()) {
-                file += databaseConfig.buildFilterUrlParams();
-            }
+    private void slurp() {
+        String lastSeq = readLastSequenceFromIndex();
 
-            if (lastSeq != null) {
-                try {
-                    file = file + "&since=" + URLEncoder.encode(lastSeq, "UTF-8");
-                } catch (UnsupportedEncodingException e) {
-                    // should not happen, but in any case...
-                    file = file + "&since=" + lastSeq;
-                }
-            }
+        String file = "/" + databaseConfig.getDatabase() + "/_changes?feed=continuous&include_docs=true&heartbeat=10000";
+        if (databaseConfig.shouldUseFilter()) {
+            file += databaseConfig.buildFilterUrlParams();
+        }
 
-            if (logger.isDebugEnabled()) {
-                logger.debug("using url [{}], path [{}]", connectionConfig.getUrl(), file);
-            }
-
-            HttpURLConnection connection = null;
-            InputStream is = null;
+        if (lastSeq != null) {
             try {
-                URL url = new URL(connectionConfig.getUrl(), file);
-                connection = (HttpURLConnection) url.openConnection();
+                file = file + "&since=" + URLEncoder.encode(lastSeq, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                // should not happen, but in any case...
+                file = file + "&since=" + lastSeq;
+            }
+        }
 
-                if (connectionConfig.requiresAuthentication()) {
-                    connection.addRequestProperty("Authorization", connectionConfig.getBasicAuthHeader());
-                }
-                connection.setDoInput(true);
-                connection.setUseCaches(false);
+        if (logger.isDebugEnabled()) {
+            logger.debug("using url [{}], path [{}]", connectionConfig.getUrl(), file);
+        }
 
-                if (!connectionConfig.shouldVerifyHostname()) {
-                    ((HttpsURLConnection) connection).setHostnameVerifier(
-                            new HostnameVerifier() {
-                                public boolean verify(String string, SSLSession ssls) {
-                                    return true;
-                                }
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        try {
+            URL url = new URL(connectionConfig.getUrl(), file);
+            connection = (HttpURLConnection) url.openConnection();
+
+            if (connectionConfig.requiresAuthentication()) {
+                connection.addRequestProperty("Authorization", connectionConfig.getBasicAuthHeader());
+            }
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+
+            if (!connectionConfig.shouldVerifyHostname()) {
+                ((HttpsURLConnection) connection).setHostnameVerifier(
+                        new HostnameVerifier() {
+                            public boolean verify(String string, SSLSession ssls) {
+                                return true;
                             }
-                    );
-                }
+                        }
+                );
+            }
 
-                is = connection.getInputStream();
+            is = connection.getInputStream();
 
-                final BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if (closed) {
-                        return;
-                    }
-                    if (line.length() == 0) {
-                        logger.trace("[couchdb] heartbeat");
-                        continue;
-                    }
-                    if (logger.isTraceEnabled()) {
-                        logger.trace("[couchdb] {}", line);
-                    }
-                    // we put here, so we block if there is no space to add
-                    stream.put(line);
-                }
-            } catch (Exception e) {
-                Closeables.closeQuietly(is);
-                if (connection != null) {
-                    try {
-                        connection.disconnect();
-                    } catch (Exception e1) {
-                        // ignore
-                    } finally {
-                        connection = null;
-                    }
-                }
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line;
+            while ((line = reader.readLine()) != null) {
                 if (closed) {
                     return;
                 }
-                logger.warn("failed to read from _changes, throttling....", e);
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e1) {
-                    if (closed) {
-                        return;
-                    }
+                if (line.length() == 0) {
+                    logger.trace("[couchdb] heartbeat");
+                    continue;
                 }
-            } finally {
-                Closeables.closeQuietly(is);
-                if (connection != null) {
-                    try {
-                        connection.disconnect();
-                    } catch (Exception e1) {
-                        // ignore
-                    } finally {
-                        connection = null;
-                    }
+                if (logger.isTraceEnabled()) {
+                    logger.trace("[couchdb] {}", line);
+                }
+                // we put here, so we block if there is no space to add
+                stream.put(line);
+            }
+        } catch (Exception e) {
+            Closeables.closeQuietly(is);
+            if (connection != null) {
+                try {
+                    connection.disconnect();
+                } catch (Exception e1) {
+                    // ignore
+                } finally {
+                    connection = null;
+                }
+            }
+            if (closed) {
+                return;
+            }
+            logger.warn("failed to read from _changes, throttling....", e);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e1) {
+                if (closed) {
+                    return;
+                }
+            }
+        } finally {
+            Closeables.closeQuietly(is);
+            if (connection != null) {
+                try {
+                    connection.disconnect();
+                } catch (Exception e1) {
+                    // ignore
+                } finally {
+                    connection = null;
                 }
             }
         }
+    }
+
+    private String readLastSequenceFromIndex() {
+        client.admin().indices().prepareRefresh(riverConfig.getRiverIndexName()).execute().actionGet();
+
+        GetResponse lastSeqResponse = client.prepareGet(riverConfig.getRiverIndexName(),
+                riverConfig.getRiverName().name(), "_seq").execute().actionGet();
+
+        if (lastSeqResponse.isExists()) {
+            return parseLastSeq(lastSeqResponse);
+        }
+        return null;
+    }
+
+    private String parseLastSeq(GetResponse lastSeqResponse) {
+        @SuppressWarnings("unchecked")
+        Map<String, Object> db = (Map) lastSeqResponse.getSourceAsMap().get(databaseConfig.getDatabase());
+        return db == null ? null : (String) db.get("last_seq");
     }
 
     public void close() {
