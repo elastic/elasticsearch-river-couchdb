c16ced9463a4152d583981f94ba12bbbbd19618c
pawelrychlik <pawel.rychlik@rspective.pl>
2013-04-15 14:57:12 +0200

    extracted Indexer class

--- src/main/java/org/elasticsearch/river/couchdb/CouchdbRiver.java
+++ src/main/java/org/elasticsearch/river/couchdb/CouchdbRiver.java
@@ -19,17 +19,13 @@
 
 package org.elasticsearch.river.couchdb;
 
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.block.ClusterBlockException;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.util.concurrent.jsr166y.LinkedTransferQueue;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.indices.IndexAlreadyExistsException;
 import org.elasticsearch.river.*;
+import org.elasticsearch.river.couchdb.kernel.index.Indexer;
 import org.elasticsearch.river.couchdb.kernel.slurp.ChangeHandler;
 import org.elasticsearch.river.couchdb.kernel.slurp.CouchdbHttpClient;
 import org.elasticsearch.river.couchdb.kernel.slurp.LastSeqReader;
@@ -37,20 +33,14 @@ import org.elasticsearch.river.couchdb.kernel.slurp.Slurper;
 import org.elasticsearch.river.couchdb.kernel.slurp.UrlBuilder;
 import org.elasticsearch.script.ExecutableScript;
 import org.elasticsearch.script.ScriptService;
-import java.io.*;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
 import static org.elasticsearch.ExceptionsHelper.unwrapCause;
-import static org.elasticsearch.client.Requests.deleteRequest;
-import static org.elasticsearch.client.Requests.indexRequest;
 import static org.elasticsearch.common.base.Throwables.propagate;
 import static org.elasticsearch.common.collect.Lists.newArrayList;
 import static org.elasticsearch.common.util.concurrent.EsExecutors.daemonThreadFactory;
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.river.couchdb.util.Sleeper.sleepLong;
 
 /**
@@ -157,187 +147,4 @@ public class CouchdbRiver extends AbstractRiverComponent implements River {
             slurper.close();
         }
     }
-
-    @SuppressWarnings({"unchecked"})
-    private Object processLine(String s, BulkRequestBuilder bulk) {
-        Map<String, Object> ctx;
-        try {
-            ctx = XContentFactory.xContent(XContentType.JSON).createParser(s).mapAndClose();
-        } catch (IOException e) {
-            logger.warn("failed to parse {}", e, s);
-            return null;
-        }
-        if (ctx.containsKey("error")) {
-            logger.warn("received error {}", s);
-            return null;
-        }
-        Object seq = ctx.get("seq");
-        String id = ctx.get("id").toString();
-
-        // Ignore design documents
-        if (id.startsWith("_design/")) {
-            if (logger.isTraceEnabled()) {
-                logger.trace("ignoring design document {}", id);
-            }
-            return seq;
-        }
-
-        if (script != null) {
-            script.setNextVar("ctx", ctx);
-            try {
-                script.run();
-                // we need to unwrap the ctx...
-                ctx = (Map<String, Object>) script.unwrap(ctx);
-            } catch (Exception e) {
-                logger.warn("failed to script process {}, ignoring", e, ctx);
-                return seq;
-            }
-        }
-
-        if (ctx.containsKey("ignore") && ctx.get("ignore").equals(Boolean.TRUE)) {
-            // ignore dock
-        } else if (ctx.containsKey("deleted") && ctx.get("deleted").equals(Boolean.TRUE)) {
-            String index = extractIndex(ctx);
-            String type = extractType(ctx);
-            if (logger.isTraceEnabled()) {
-                logger.trace("processing [delete]: [{}]/[{}]/[{}]", index, type, id);
-            }
-            bulk.add(deleteRequest(index).type(type).id(id).routing(extractRouting(ctx)).parent(extractParent(ctx)));
-        } else if (ctx.containsKey("doc")) {
-            String index = extractIndex(ctx);
-            String type = extractType(ctx);
-            Map<String, Object> doc = (Map<String, Object>) ctx.get("doc");
-
-            // Remove _attachment from doc if needed
-            if (databaseConfig.shouldIgnoreAttachments()) {
-                // no need to log that we removed it, the doc indexed will be shown without it
-                doc.remove("_attachments");
-            } else {
-                // TODO by now, couchDB river does not really store attachments but only attachments meta infomration
-                // So we perhaps need to fully support attachments
-            }
-
-            if (logger.isTraceEnabled()) {
-                logger.trace("processing [index ]: [{}]/[{}]/[{}], source {}", index, type, id, doc);
-            }
-
-            bulk.add(indexRequest(index).type(type).id(id).source(doc).routing(extractRouting(ctx)).parent(extractParent(ctx)));
-        } else {
-            logger.warn("ignoring unknown change {}", s);
-        }
-        return seq;
-    }
-
-    private String extractParent(Map<String, Object> ctx) {
-        return (String) ctx.get("_parent");
-    }
-
-    private String extractRouting(Map<String, Object> ctx) {
-        return (String) ctx.get("_routing");
-    }
-
-    private String extractType(Map<String, Object> ctx) {
-        String type = (String) ctx.get("_type");
-        if (type == null) {
-            type = indexConfig.getType();
-        }
-        return type;
-    }
-
-    private String extractIndex(Map<String, Object> ctx) {
-        String index = (String) ctx.get("_index");
-        if (index == null) {
-            index = indexConfig.getName();
-        }
-        return index;
-    }
-
-    private class Indexer implements Runnable {
-        @Override
-        public void run() {
-            while (true) {
-                if (closed) {
-                    return;
-                }
-                String s;
-                try {
-                    s = stream.take();
-                } catch (InterruptedException e) {
-                    if (closed) {
-                        return;
-                    }
-                    continue;
-                }
-                BulkRequestBuilder bulk = client.prepareBulk();
-                Object lastSeq = null;
-                Object lineSeq = processLine(s, bulk);
-                if (lineSeq != null) {
-                    lastSeq = lineSeq;
-                }
-
-                // spin a bit to see if we can get some more changes
-                try {
-                    while ((s = stream.poll(indexConfig.getBulkTimeout().millis(), TimeUnit.MILLISECONDS)) != null) {
-                        lineSeq = processLine(s, bulk);
-                        if (lineSeq != null) {
-                            lastSeq = lineSeq;
-                        }
-
-                        if (bulk.numberOfActions() >= indexConfig.getBulkSize()) {
-                            break;
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    if (closed) {
-                        return;
-                    }
-                }
-
-                if (lastSeq != null) {
-                    try {
-                        // we always store it as a string
-                        String lastSeqAsString = null;
-                        if (lastSeq instanceof List) {
-                            // bigcouch uses array for the seq
-                            try {
-                                XContentBuilder builder = XContentFactory.jsonBuilder();
-                                //builder.startObject();
-                                builder.startArray();
-                                for (Object value : ((List) lastSeq)) {
-                                    builder.value(value);
-                                }
-                                builder.endArray();
-                                //builder.endObject();
-                                lastSeqAsString = builder.string();
-                            } catch (Exception e) {
-                                logger.error("failed to convert last_seq to a json string", e);
-                            }
-                        } else {
-                            lastSeqAsString = lastSeq.toString();
-                        }
-                        if (logger.isTraceEnabled()) {
-                            logger.trace("processing [_seq  ]: [{}]/[{}]/[{}], last_seq [{}]", riverIndexName, riverName.name(), "_seq", lastSeqAsString);
-                        }
-                        bulk.add(indexRequest(riverIndexName).type(riverName.name()).id("_seq")
-                                .source(jsonBuilder().startObject().startObject("couchdb").field("last_seq", lastSeqAsString).endObject().endObject()));
-                    } catch (IOException e) {
-                        logger.warn("failed to add last_seq entry to bulk indexing");
-                    }
-                }
-
-                try {
-                    BulkResponse response = bulk.execute().actionGet();
-                    if (response.hasFailures()) {
-                        // TODO write to exception queue?
-                        logger.warn("failed to execute" + response.buildFailureMessage());
-                    }
-                } catch (Exception e) {
-                    logger.warn("failed to execute bulk", e);
-                }
-            }
-        }
-    }
-
-
-
 }
---
+++ src/main/java/org/elasticsearch/river/couchdb/kernel/index/Indexer.java
@@ -0,0 +1,216 @@
+package org.elasticsearch.river.couchdb.kernel.index;
+
+import static org.elasticsearch.client.Requests.deleteRequest;
+import static org.elasticsearch.client.Requests.indexRequest;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.river.couchdb.CouchdbDatabaseConfig;
+import org.elasticsearch.river.couchdb.IndexConfig;
+import org.elasticsearch.river.couchdb.RiverConfig;
+import org.elasticsearch.script.ExecutableScript;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class Indexer implements Runnable {
+
+    private ESLogger logger;
+
+    private BlockingQueue<String> stream;
+    private Client client;
+
+    private IndexConfig indexConfig;
+    private CouchdbDatabaseConfig databaseConfig;
+    private RiverConfig riverConfig;
+    private ExecutableScript script;
+
+    private volatile boolean closed;
+
+    @Override
+    public void run() {
+        while (true) {
+            if (closed) {
+                return;
+            }
+            String s;
+            try {
+                s = stream.take();
+            } catch (InterruptedException e) {
+                if (closed) {
+                    return;
+                }
+                continue;
+            }
+            BulkRequestBuilder bulk = client.prepareBulk();
+            Object lastSeq = null;
+            Object lineSeq = processLine(s, bulk);
+            if (lineSeq != null) {
+                lastSeq = lineSeq;
+            }
+
+            // spin a bit to see if we can get some more changes
+            try {
+                while ((s = stream.poll(indexConfig.getBulkTimeout().millis(), TimeUnit.MILLISECONDS)) != null) {
+                    lineSeq = processLine(s, bulk);
+                    if (lineSeq != null) {
+                        lastSeq = lineSeq;
+                    }
+
+                    if (bulk.numberOfActions() >= indexConfig.getBulkSize()) {
+                        break;
+                    }
+                }
+            } catch (InterruptedException e) {
+                if (closed) {
+                    return;
+                }
+            }
+
+            if (lastSeq != null) {
+                try {
+                    // we always store it as a string
+                    String lastSeqAsString = null;
+                    if (lastSeq instanceof List) {
+                        // bigcouch uses array for the seq
+                        try {
+                            XContentBuilder builder = XContentFactory.jsonBuilder();
+                            //builder.startObject();
+                            builder.startArray();
+                            for (Object value : ((List) lastSeq)) {
+                                builder.value(value);
+                            }
+                            builder.endArray();
+                            //builder.endObject();
+                            lastSeqAsString = builder.string();
+                        } catch (Exception e) {
+                            logger.error("failed to convert last_seq to a json string", e);
+                        }
+                    } else {
+                        lastSeqAsString = lastSeq.toString();
+                    }
+                    if (logger.isTraceEnabled()) {
+                        logger.trace("processing [_seq  ]: [{}]/[{}]/[{}], last_seq [{}]",
+                                riverConfig.getRiverIndexName(), riverConfig.getRiverName().name(), "_seq", lastSeqAsString);
+                    }
+                    bulk.add(indexRequest(riverConfig.getRiverIndexName()).type(riverConfig.getRiverName().name()).id("_seq")
+                            .source(jsonBuilder().startObject().startObject("couchdb").field("last_seq", lastSeqAsString).endObject().endObject()));
+                } catch (IOException e) {
+                    logger.warn("failed to add last_seq entry to bulk indexing");
+                }
+            }
+
+            try {
+                BulkResponse response = bulk.execute().actionGet();
+                if (response.hasFailures()) {
+                    // TODO write to exception queue?
+                    logger.warn("failed to execute" + response.buildFailureMessage());
+                }
+            } catch (Exception e) {
+                logger.warn("failed to execute bulk", e);
+            }
+        }
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private Object processLine(String s, BulkRequestBuilder bulk) {
+        Map<String, Object> ctx;
+        try {
+            ctx = XContentFactory.xContent(XContentType.JSON).createParser(s).mapAndClose();
+        } catch (IOException e) {
+            logger.warn("failed to parse {}", e, s);
+            return null;
+        }
+        if (ctx.containsKey("error")) {
+            logger.warn("received error {}", s);
+            return null;
+        }
+        Object seq = ctx.get("seq");
+        String id = ctx.get("id").toString();
+
+        // Ignore design documents
+        if (id.startsWith("_design/")) {
+            if (logger.isTraceEnabled()) {
+                logger.trace("ignoring design document {}", id);
+            }
+            return seq;
+        }
+
+        if (script != null) {
+            script.setNextVar("ctx", ctx);
+            try {
+                script.run();
+                // we need to unwrap the ctx...
+                ctx = (Map<String, Object>) script.unwrap(ctx);
+            } catch (Exception e) {
+                logger.warn("failed to script process {}, ignoring", e, ctx);
+                return seq;
+            }
+        }
+
+        if (ctx.containsKey("ignore") && ctx.get("ignore").equals(Boolean.TRUE)) {
+            // ignore dock
+        } else if (ctx.containsKey("deleted") && ctx.get("deleted").equals(Boolean.TRUE)) {
+            String index = extractIndex(ctx);
+            String type = extractType(ctx);
+            if (logger.isTraceEnabled()) {
+                logger.trace("processing [delete]: [{}]/[{}]/[{}]", index, type, id);
+            }
+            bulk.add(deleteRequest(index).type(type).id(id).routing(extractRouting(ctx)).parent(extractParent(ctx)));
+        } else if (ctx.containsKey("doc")) {
+            String index = extractIndex(ctx);
+            String type = extractType(ctx);
+            Map<String, Object> doc = (Map<String, Object>) ctx.get("doc");
+
+            // Remove _attachment from doc if needed
+            if (databaseConfig.shouldIgnoreAttachments()) {
+                // no need to log that we removed it, the doc indexed will be shown without it
+                doc.remove("_attachments");
+            } else {
+                // TODO by now, couchDB river does not really store attachments but only attachments meta infomration
+                // So we perhaps need to fully support attachments
+            }
+
+            if (logger.isTraceEnabled()) {
+                logger.trace("processing [index ]: [{}]/[{}]/[{}], source {}", index, type, id, doc);
+            }
+
+            bulk.add(indexRequest(index).type(type).id(id).source(doc).routing(extractRouting(ctx)).parent(extractParent(ctx)));
+        } else {
+            logger.warn("ignoring unknown change {}", s);
+        }
+        return seq;
+    }
+
+    private String extractParent(Map<String, Object> ctx) {
+        return (String) ctx.get("_parent");
+    }
+
+    private String extractRouting(Map<String, Object> ctx) {
+        return (String) ctx.get("_routing");
+    }
+
+    private String extractType(Map<String, Object> ctx) {
+        String type = (String) ctx.get("_type");
+        if (type == null) {
+            type = indexConfig.getType();
+        }
+        return type;
+    }
+
+    private String extractIndex(Map<String, Object> ctx) {
+        String index = (String) ctx.get("_index");
+        if (index == null) {
+            index = indexConfig.getName();
+        }
+        return index;
+    }
+}
+
