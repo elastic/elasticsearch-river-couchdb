83878d8de4bba1e96eb81b81eb206474a6a131aa
pawelrychlik <pawel.rychlik@rspective.pl>
2013-04-20 00:33:56 +0200

    refactored main() for debugging

--- src/test/java/org/elasticsearch/river/couchdb/CouchdbRiverTest.java
+++ src/test/java/org/elasticsearch/river/couchdb/CouchdbRiverTest.java
@@ -1,38 +1,56 @@
 package org.elasticsearch.river.couchdb;
 
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.indices.IndexMissingException;
 import org.elasticsearch.node.Node;
 import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+import java.io.IOException;
 
 public class CouchdbRiverTest {
 
     public static void main(String[] args) throws Exception {
-        Node node = nodeBuilder().settings(settingsBuilder().put("gateway.type", "local")).node();
-        Thread.sleep(1000);
+        new CouchdbRiverTest().run();
+    }
 
-        XContentBuilder src = jsonBuilder().startObject()
-                    .field("type", "couchdb")
-                    .startObject("index")
-                        .field("name", "db")
-                        .field("type", "couchdb")
-                        .field("ignore_attachments", true)
-                    .endObject()
-                    .startObject("couchdb_connection")
-                        .field("url", "http://localhost:5984")
-                    .endObject()
-                    .startObject("couchdb_database")
-                        .field("database", "db")
-                    .endObject()
-                .endObject();
+    private Node node;
+
+    private String river = "_river";
+    private String type = "couchdb";
+    private String db = "db";
+    private String index = db;
+
+    private void run() throws Exception {
+        node = nodeBuilder().settings(settingsBuilder().put("gateway.type", "local")).node();
+
+        delete(river);
+        delete(db);
 
         try {
-            node.client().prepareIndex("_river", "couchdb", "_meta").setSource(src).execute().actionGet();
+            node.client().prepareIndex("_river", "couchdb", "_meta").setSource(meta()).execute().actionGet();
 
             Thread.sleep(1000000);
         } finally {
             node.stop();
         }
+    }
+
+    private XContentBuilder meta() throws IOException {
+        return jsonBuilder().startObject()
+                    .field("type", type)
+                    .startObject("index").field("name", index).field("type", type).field("ignore_attachments", true).endObject()
+                    .startObject("couchdb_connection").field("url", "http://localhost:5984").endObject()
+                    .startObject("couchdb_database").field("database", db).endObject()
+                    .endObject();
+    }
+
+    private void delete(String resource) throws InterruptedException {
+        try {
+            node.client().admin().indices().delete(new DeleteIndexRequest(resource)).actionGet();
+        } catch (IndexMissingException itsOk) {}
+
+        Thread.sleep(100);
     }
 }
