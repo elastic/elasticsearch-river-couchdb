31d75200bffad7ba3d2c35e1a1c73f821bddbb43
pawelrychlik <pawel.rychlik@rspective.pl>
2013-04-20 00:10:23 +0200

    main() useful for debugging

--- src/test/java/org/elasticsearch/river/couchdb/CouchdbRiverTest.java
+++ src/test/java/org/elasticsearch/river/couchdb/CouchdbRiverTest.java
@@ -1,40 +1,38 @@
-/*
- * Licensed to ElasticSearch and Shay Banon under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. ElasticSearch licenses this
- * file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package org.elasticsearch.river.couchdb;
 
-import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.node.Node;
-import org.elasticsearch.node.NodeBuilder;
-
+import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
-/**
- *
- */
 public class CouchdbRiverTest {
 
     public static void main(String[] args) throws Exception {
-        Node node = NodeBuilder.nodeBuilder().settings(ImmutableSettings.settingsBuilder().put("gateway.type", "local")).node();
+        Node node = nodeBuilder().settings(settingsBuilder().put("gateway.type", "local")).node();
         Thread.sleep(1000);
-        node.client().prepareIndex("_river", "db", "_meta").setSource(jsonBuilder().startObject().field("type", "couchdb").endObject()).execute().actionGet();
 
-        Thread.sleep(1000000);
+        XContentBuilder src = jsonBuilder().startObject()
+                    .field("type", "couchdb")
+                    .startObject("index")
+                        .field("name", "db")
+                        .field("type", "couchdb")
+                        .field("ignore_attachments", true)
+                    .endObject()
+                    .startObject("couchdb_connection")
+                        .field("url", "http://localhost:5984")
+                    .endObject()
+                    .startObject("couchdb_database")
+                        .field("database", "db")
+                    .endObject()
+                .endObject();
+
+        try {
+            node.client().prepareIndex("_river", "couchdb", "_meta").setSource(src).execute().actionGet();
+
+            Thread.sleep(1000000);
+        } finally {
+            node.stop();
+        }
     }
 }
